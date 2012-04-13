448545a3f97ebf3b4350cbcfe4439b44874cb91a
Richard Carter <richard@richardcarter.org>
2012-04-13 12:46:02 -0400

    draw test image

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 window.CANVAS_WIDTH = 800;
 window.CANVAS_HEIGHT = 600;
 
-require(['gfx', 'tileset'], function(gfx, tileset) {
+require(['gfx', 'tileset', 'images'], function(gfx, tileset, images) {
 
 	gfx.loadCanvas('maincanvas');
 	tileset.loadTileset('test');
+    images.loadImage('img/dude.png');
 
     (function drawLoop() {
     	requestAnimFrame(drawLoop);
@@ -14,9 +15,11 @@ require(['gfx', 'tileset'], function(gfx, tileset) {
     	if(tileset.loadedTileset('test')) {
     		// TODO obviously need to cache these values
             var whichGlider = Math.floor((now / 500) % 4);
-            console.log(whichGlider);
             var tile = tileset.getTile('test', 'glider' + whichGlider);
     		gfx.drawTile(tile, 0, 0);
     	}
+        if(images.isLoaded('img/dude.png')) {
+            gfx.drawImage('img/dude.png', 32, 0);
+        }
     })();
 });
