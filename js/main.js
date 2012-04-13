30a8aba594a5f3032d9d93551d8caaf0bbf94749
Richard Carter <richard@richardcarter.org>
2012-04-13 00:22:46 -0400

    Add canvas draw loop and fallback message

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 require([], function() {
-    console.log('Hello world');
+    var canvas, ctx;
+
+    canvas = document.getElementById('maincanvas');
+    ctx = canvas.getContext('2d');
+
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+
+    (function drawLoop() {
+    	requestAnimFrame(drawLoop);
+    	// render here
+    })();
 });
