c593bc3369537e8e3223fa8dc685cc8fd3077997
Richard Carter <richard@richardcarter.org>
2012-04-13 12:44:24 -0400

    advice added to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+# Make sure to disable caching in your browser
+# In Chrome you can find this in the developer tools settings
+# (you have to leave developer tools open for it to apply)
+
 python -m SimpleHTTPServer 8081
 
+# You can also use heel; install with `gem install heel`
+#heel
