8e98d616348da5b45798ceebcc2941663f516297
Richard Carter <richard@richardcarter.org>
2012-04-13 12:49:21 -0400

    images.loadImage now caches instead of requesting a new image repeatedly

--- js/images.js
+++ js/images.js
@@ -3,6 +3,20 @@ define([], function() {
 
 	return {
 		loadImage : function (url, oncomplete) {
+			if(url in images) {
+				if(oncomplete) {
+					if(images[url].complete) {
+						oncomplete();
+					} else {
+						// note this overrides the complete callback; no easy way to add multiple
+						// callbacks, and it probably would not be the desired behavior anyway
+						images[url].onload = oncomplete;
+					}
+				}
+				
+				return images[url];
+			}
+
 			var newimg = new Image();
 			if(oncomplete) {
 				newimg.onload = oncomplete;
