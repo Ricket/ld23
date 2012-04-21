a238c657e09ba907f29078b992b53e45dc86d0fd
Richard Carter <richard@richardcarter.org>
2012-04-21 16:10:04 -0400

    fill out getNearestPointOnCircle

--- js/math.js
+++ js/math.js
@@ -1,7 +1,13 @@
 define([], function() {
 	return {
 		getNearestPointOnCircle: function(x, y, circlex, circley, radius) {
-			return [x, y];
+			var vecx = x - circlex,
+			    vecy = y - circley;
+
+			var mag = Math.sqrt(vecx * vecx + vecy * vecy);
+			vecx = vecx / mag * radius;
+			vecy = vecy / mag * radius;
+			return [circlex + vecx, circley + vecy];
 		}
 	};
 });
